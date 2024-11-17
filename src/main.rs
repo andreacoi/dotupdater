7fb7c4106f019d69cccb3b5d41896be87c1d40eb
Andrea Coi <andrea.coi@outlook.it>
2024-11-17 22:42:13 +0100

    create pull function and log system by date - Xorg.log style

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use chrono::prelude::*;
 use git2::{Error, FetchOptions, RemoteCallbacks, Repository};
 use serde::Deserialize;
+use std::fmt::write;
 use std::fs;
 use std::io::Write;
 use std::net::TcpStream;
@@ -57,7 +59,39 @@ fn fetch(repo_path: &str, branch: &str) -> Result<(), Error> {
     if analysis.is_up_to_date() {
         Ok(())
     } else {
-        Err(Error::from_str("Repository has updates available"))
+        Err(Error::from_str("Repository has updates available!"))
+    }
+}
+
+// pull function - useful only when fetch returns an error
+fn pull(repo_path: &str, branch: &str) -> Result<(), Error> {
+    let repo = Repository::open(repo_path)?;
+    let mut remote = repo.find_remote("origin")?;
+    // create a FetchOption object in order to perform auth
+    let mut fetch_options = FetchOptions::new();
+    // prepare fetch to use callbacks
+    fetch_options.remote_callbacks(create_credentials_callback());
+    // execute fetch
+    remote.fetch(&[branch], Some(&mut fetch_options), None)?;
+
+    // check head and commit
+    let fetch_head = repo.find_reference("FETCH_HEAD")?;
+    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
+    let (analysis, _) = repo.merge_analysis(&[&fetch_commit])?;
+    if analysis.is_up_to_date() {
+        return Ok(());
+    }
+    if analysis.is_fast_forward() {
+        let mut branch = repo.find_branch(branch, git2::BranchType::Local)?;
+        let mut branch_ref = branch.into_reference();
+        branch_ref.set_target(fetch_commit.id(), "Fast Forward")?;
+        repo.set_head(branch_ref.name().unwrap())?;
+        repo.checkout_head(None)?;
+        Ok(())
+    } else {
+        Err(Error::from_str(
+            "Merge conflict or other reason, skipping pull",
+        ))
     }
 }
 
@@ -73,6 +107,12 @@ fn wait_for_connection() {
         sleep(Duration::from_secs(3));
     }
 }
+// create useful function for log. returns a string to be placed in log file.
+fn get_task_datetime() -> String {
+    let local: DateTime<Local> = Local::now();
+    let formatted_date = local.format("%Y-%m-%d %H:%M:%S").to_string();
+    formatted_date
+}
 
 // I moved the pull function inside the main function to feel more confident and to make it easier
 // to pass arguments and log the corresponding output.
@@ -86,15 +126,64 @@ fn main() {
     // bind config.config.config_base_path to the variable config_base_path
     let config_base_path: String = config.config_base_path;
     // same story for logfiles_path
-    let logfiles_path: String = config.log_path;
+    let logfiles_path: String = format!("{}/{}", config.log_path, "logfile.log");
     // create log file if not exists, otherwise append logs to that file.
     let mut log_file = fs::OpenOptions::new()
         .create(true)
         .append(true)
         .open(logfiles_path)
         .expect("Cannot open log file.");
-    // iterate over each repo in config files
-    for repo_config in &config.repositories {}
     // check_internet_connection
     wait_for_connection();
+    // iterate over each repo in config files
+    for repo_config in &config.repositories {
+        let full_path: String = format!("{}{}", config_base_path, repo_config.path);
+        let log_date: String = get_task_datetime();
+        // write which repo is processing with date time information
+        writeln!(
+            log_file,
+            "[I] {} - Processing repository: {}",
+            log_date, full_path
+        )
+        .unwrap();
+        // start to fetch at every repo
+        match fetch(&full_path, &repo_config.branch) {
+            Ok(_) => {
+                let log_date: String = get_task_datetime();
+                // if fetch result is: up-to-date skip and write process to logfile.
+                writeln!(
+                    log_file,
+                    "[!] {} - No updates for branch {}",
+                    log_date, &repo_config.branch
+                );
+            }
+            Err(e) => {
+                // fetch method retrieves some update, behave accordingly but, write on log.
+                let log_date: String = get_task_datetime();
+                writeln!(
+                    log_file,
+                    "[II] {} - Fetching updates for {}: {}",
+                    log_date, &repo_config.branch, e
+                );
+                match pull(&full_path, &repo_config.branch) {
+                    Ok(_) => {
+                        let log_date: String = get_task_datetime();
+                        writeln!(
+                            log_file,
+                            "[U] {} - Pulling updates for {}",
+                            log_date, &repo_config.branch
+                        );
+                    }
+                    Err(e) => {
+                        let log_date: String = get_task_datetime();
+                        writeln!(
+                            log_file,
+                            "[E] {} - Failed to pulling updates for {}: {}",
+                            log_date, &repo_config.branch, e
+                        );
+                    }
+                }
+            }
+        }
+    }
 }
