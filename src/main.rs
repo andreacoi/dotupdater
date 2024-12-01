34292954bce5090bc8d30c6ac99040ff84b1e52b
Andrea Coi <andrea.coi@outlook.it>
2024-12-01 17:25:31 +0100

    insert tip to use module init in main function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use dirs;
-use dotupdater::github_utils::{fetch, pull};
-use dotupdater::init;
-use dotupdater::logger;
-use dotupdater::utils::wait_for_connection;
-use dotupdater::Config;
-use dotupdater::RepositoryConfig;
-
-// I moved the pull function outside the main function to feel more confident and to make it easier
-// to pass arguments and log the corresponding output.
-fn main() {}
+// tip to use module init
+//use dotupdater::init::initialize;
+fn main() {
+    //initialize();
+}
