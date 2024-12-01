efe8ebe7f32f533686abf622f163e273619a4072
Andrea Coi <andrea.coi@outlook.it>
2024-12-01 17:18:25 +0100

    finalize splitting function in submodules autowiring by lib.rs

--- src/init.rs
+++ src/init.rs
@@ -4,57 +4,39 @@
 // not initialize_log
 // else
 // initialize element that aren't initialized
-
+use crate::appvars::{get_complete_log_file_path, LOGFILE};
+use crate::config::{create_base_config_path, create_blueprint_config_file};
+use crate::logger::{create_base_logfiles_path, create_log_file, logevent, EventType};
 // this function create all the enviroment.
 pub fn initialize() {
     // create log folder
-    logger::create_base_logfiles_path();
+    create_base_logfiles_path();
     // create log file
-    logger::create_log_file();
-    let logfile_path = format!(
-        "{}{}",
-        appvars::get_complete_log_file_path(),
-        appvars::LOGFILE
-    );
+    create_log_file();
+    let logfile_path = format!("{}{}", get_complete_log_file_path(), LOGFILE);
 
     // log starting app
     logevent(
         &logfile_path,
         String::from("Starting app..."),
-        logger::EventType::I(String::from("[I]")),
+        EventType::I(String::from("[I]")),
     );
 
     match create_base_config_path() {
         Ok(message) => {
-            logevent(
-                &logfile_path,
-                message,
-                logger::EventType::I(String::from("[I]")),
-            );
+            logevent(&logfile_path, message, EventType::I(String::from("[I]")));
         }
         Err(message) => {
-            logevent(
-                &logfile_path,
-                message,
-                logger::EventType::E(String::from("[E]")),
-            );
+            logevent(&logfile_path, message, EventType::E(String::from("[E]")));
         }
     }
 
     match create_blueprint_config_file() {
         Ok(message) => {
-            logevent(
-                &logfile_path,
-                message,
-                logger::EventType::I(String::from("[I]")),
-            );
+            logevent(&logfile_path, message, EventType::I(String::from("[I]")));
         }
         Err(message) => {
-            logevent(
-                &logfile_path,
-                message,
-                logger::EventType::E(String::from("[E]")),
-            );
+            logevent(&logfile_path, message, EventType::E(String::from("[E]")));
         }
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
-pub mod utils {
-    // check internet connection
-    use std::net::TcpStream;
-    use std::thread::sleep;
-    use std::time::Duration;
-    fn check_internet_connection() -> bool {
-        TcpStream::connect("8.8.8.8:53").is_ok()
-    }
-
-    // function to implement an infinite cycle if the computer is connected. while the computer is not
-    // connected sleep for 3 second then re-check the connection.
-    pub fn wait_for_connection() {
-        while !check_internet_connection() {
-            sleep(Duration::from_secs(3));
-        }
-    }
-}
+pub mod appvars;
+pub mod config;
+pub mod ghutils;
+pub mod init;
+pub mod logger;
+pub mod utils;
