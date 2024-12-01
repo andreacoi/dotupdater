84f2ea7aed7229dce1c8e3f667a75b4bdfd4b2d2
Andrea Coi <andrea.coi@outlook.it>
2024-12-01 19:49:35 +0100

    start to porting all the main functions of the program in the main file. testing the first logging cycling the repo folder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 // tip to use module init
 //use dotupdater::init::initialize;
-use dotupdater::ghutils::{self, get_config_list};
+use dotupdater::{
+    appvars::prepend_dir,
+    ghutils::{self, get_config_list},
+    init::initialize,
+    logger::{logevent, EventType},
+};
 fn main() {
+    initialize();
     let config = get_config_list();
+    // cycle every config in the file config.toml
+    for repo_config in &config.repositories {
+        let full_path: String = prepend_dir(&repo_config.path);
+        logevent(
+            format!("Inspecting folder for a GitHub repo: {}", &full_path),
+            EventType::I(String::from("[I]")),
+        );
+    }
 }
