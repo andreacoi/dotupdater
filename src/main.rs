82b8baf50212a3df98115c2fc07dbb67fe5f4c35
Andrea Coi <andrea.coi@outlook.it>
2024-12-01 19:13:28 +0100

    start to assemble all the components in the main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // tip to use module init
 //use dotupdater::init::initialize;
+use dotupdater::ghutils::{self, get_config_list};
 fn main() {
-    //initialize();
+    let config = get_config_list();
 }
